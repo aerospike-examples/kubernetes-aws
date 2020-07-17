7b0a7dd0b7fb55b43c9fb79837637d761ddfd070
Ken Tune <ktune@aerospike.com>
2020-07-17 20:09:20 +0100

    Fine tuning

--- install-pre-requisites.sh
+++ install-pre-requisites.sh
@@ -23,6 +23,7 @@ gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cl
 EOF"
 
 print_comment "First we must configure yum to use https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64 as a repo"
+wait_for_space_press
 exec_command "$KUBECTL_INSTALL_COMMAND"
 exec_command "sudo mv /tmp/kubernetes.repo /etc/yum.repos.d/kubernetes.repo"
 exec_command "sudo yum install -y kubectl"
@@ -30,18 +31,19 @@ exec_command "sudo yum install -y kubectl"
 # Install aws command line utility
 # Instructions from https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html
 print_comment "Installing AWS CLI tool using instructions from https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html"
+wait_for_space_press
 cd /tmp
 exec_command 'curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"'
 exec_command "unzip awscliv2.zip"
 exec_command "sudo ./aws/install"
 
 print_comment "Installing eksctl - command line tool for creating AWS Kubernetes clusters using instructions from https://docs.aws.amazon.com/eks/latest/userguide/getting-started-eksctl.html"
-
+wait_for_space_press
 exec_command 'curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp'
 exec_command 'sudo mv /tmp/eksctl /usr/local/bin'
 
 print_comment 'Installing helm using instructions from https://helm.sh/docs/intro/install'
-
+wait_for_space_press
 exec_command "curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3"
 exec_command "chmod 700 get_helm.sh"
 exec_command "./get_helm.sh"
