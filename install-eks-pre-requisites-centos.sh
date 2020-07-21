0a9adc78dd506699581f555b699c94d9d18b24c3
Ken Tune <ktune@aerospike.com>
2020-07-21 17:52:06 +0100

    centos install script

--- install-eks-pre-requisites-centos.sh
+++ install-eks-pre-requisites-centos.sh
@@ -4,7 +4,6 @@
 echo "Installing kubectl, AWS Command Line Utility, eksctl and helm"
 echo
 
-# kubectl install
 
 # First we need to configure yum to use https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64 as a repo"
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
@@ -17,6 +16,7 @@ repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 
+# kubectl install
 yum install -y kubectl
 
 # AWS CLI Install
@@ -32,6 +32,9 @@ sudo ./aws/install
 # eksctl install
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 mv /tmp/eksctl /usr/local/bin
+
+# Helm installed into /usr/local/bin - make sure this is in sudo path when checking install successful
+PATH=$PATH:/usr/local/bin
 
 # helm install
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
